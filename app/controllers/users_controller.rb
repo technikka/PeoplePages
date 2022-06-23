4c7d75a43b57f2e51c66c331ff35a11c4a77d887
Pythonikk <nikkibrusoe@gmail.com>
2022-06-23 14:49:27 -0700

    Add query functionality on user index page; query is workign turbo frame.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def search
+    if params[:query].include?('@')
+      @queried_user = User.find_by(email: params[:query])
+    else
+      profile = Profile.find_by(name: params[:query])
+      @queried_user = profile.user if profile
+    end
+
+    render partial: 'search'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[show index] do
+    get 'search', on: :collection
     resource :profile, only: %i[new create edit update]
   end
 
+  get 'search', to: 'users#search'
   post 'users/:id', to: 'profiles#create'
   get 'users/:id/profile', to: 'users#show'
 
