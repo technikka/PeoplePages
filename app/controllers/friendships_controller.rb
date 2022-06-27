d97bea6de8fbc4a174608a06741b66da7924435a
Pythonikk <nikkibrusoe@gmail.com>
2022-06-27 10:56:59 -0700

    Move notification handeling out of controller into model callbacks

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -10,12 +10,7 @@ class FriendshipsController < ApplicationController
     @friendship = Friendship.new(user_id: current_user.id, friend_id: @user.id)
 
     if @friendship.save
-      @notification = Notification.new(kind: 'friend_request', user_id: @user.id)
-      if @notification.save
-        render partial: 'cancel_request'
-      else
-        render partial: 'initiate_request'
-      end
+      render partial: 'cancel_request'
     else
       redirect_to @user, alert: 'Something went wrong requesting friend'
     end
@@ -23,12 +18,7 @@ class FriendshipsController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-
     Friendship.find_by(user_id: current_user.id, friend_id: @user.id).destroy
-
-    # This logic should be in the model dependent
-    Notification.find_by(user_id: @user.id, kind: 'friend_request').destroy
-
     render partial: 'initiate_request'
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,17 @@ class Friendship < ApplicationRecord
   validates :user_id, presence: true, uniqueness: true
   validates :friend_id, presence: true
 
+  after_save :create_notification
+  after_destroy :destroy_notification
+
+  private
+
+  def create_notification
+    Notification.create(kind: 'friend_request', user_id: self.friend_id)
+  end
+
+  def destroy_notification
+    Notification.find_by(user_id: self.friend_id, kind: 'friend_request').destroy
+  end
+
 end
