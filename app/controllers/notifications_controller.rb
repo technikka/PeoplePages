a759d091445c741094329b190d1e1028f3b52ff1
Pythonikk <nikkibrusoe@gmail.com>
2022-06-30 12:42:22 -0700

    Add deletion of notification for friend request once request is accepted, and fixed notification being created for acceptance.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,5 @@ class NotificationsController < ApplicationController
   def index
     @notifications = User.notifications(current_user)
     @friend_requests = @notifications.select { |n| n.kind_of? Friendship } if @notifications
-    binding.pry
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,6 +6,23 @@ class Friendship < ApplicationRecord
   validates :user_id, presence: true
   validates :friend_id, presence: true
 
-  after_create { create_notification }
+  after_create do
+    if mutual_relationship?
+      # find friendship for corresponding request
+      friendship = Friendship.find_by(user_id: friend.id, friend_id: user.id)
+      # delete the notification for request
+      friendship.notification.destroy
+    else
+      create_notification
+    end
+  end
+
   before_destroy { notification.destroy }
+
+  private
+
+  # determines if the creation of self is a friend request acceptance
+  def mutual_relationship?
+    friend.friends.include?(user)
+  end
 end
