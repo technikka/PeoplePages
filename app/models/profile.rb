3e446df2f047f84cb74ca3ff54bd832f374f887c
Pythonikk <nikkibrusoe@gmail.com>
2022-07-15 14:21:48 -0700

    Add functionality for uploading profile file

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,10 @@ class Profile < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :user
   has_one_attached :image
+
+  def image_as_thumbnail
+    return unless image.content_type.in?(%w[image/jpeg image/png])
+
+    image.variant(resize_to_limit: [200, 200]).processed
+  end
 end
