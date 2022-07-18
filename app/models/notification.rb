4896c0b6a1e928e092e2ee69d9a52781b40947b4
Pythonikk <nikkibrusoe@gmail.com>
2022-07-18 11:35:58 -0700

    Add validations to Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,7 +3,7 @@ class Notification < ApplicationRecord
   belongs_to :like, optional: true
   belongs_to :post, optional: true
   belongs_to :comment, optional: true
-  # validates :friend_id, presence: true, unless: :like_id || :post_id
-  # validates :like_id, presence: true, unless: :friend_id || :post_id
-  # validates :post_id, presence: true, unless: :friend_id || :like_id
+  validates :friendship_id, presence: true, unless: :like_id || :post_id
+  validates :like_id, presence: true, unless: :friendship_id || :post_id
+  validates :post_id, presence: true, unless: :friendship_id || :like_id
 end
